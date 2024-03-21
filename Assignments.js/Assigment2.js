8e65578a62b03c63975f51e1c059b8d2e80454c2
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-21 21:43:55 +0530

    Write an arrow funtion named isEven() that takes an sigle number as an argument and returns if it is even or not

---
+++ Assignments.js/Assigment2.js
@@ -0,0 +1,11 @@
+//"Write an arrow funtion named isEven() that takes an sigle number as an argument and returns if it is even or not"
+
+let isEven=(n)=>{
+    if(n%2==0){
+        console.log(`${n} is even number`);
+    }
+    else{
+        console.log(`${n} is odd number`);
+    }
+}
+isEven(2)
